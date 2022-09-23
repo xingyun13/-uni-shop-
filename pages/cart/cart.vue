0c987b82e36149422ae38b751ef6e51af06b512d
彭帅 <2077796713@qq.com>
2022-09-23 10:49:38 +0800

    完成了tabbar功能的开发

---
+++ pages/cart/cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    
+  </view>
+</template>
+
+<script lang="scss">
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
